1e67e1c7f43678a8a0f39074f53820ff73e2cd2d
reyes2981 <noe.reyes92@yahoo.com>
2021-03-14 22:53:26 -0500

    commit tree b3207e5a7bb34f62eb831ad777aeddf140812de1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190418213203) do
+ActiveRecord::Schema.define(version: 20160119185429) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -21,10 +21,10 @@ ActiveRecord::Schema.define(version: 20190418213203) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.string   "description"
-    t.integer  "author_id"
+    t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "author_id"
   end
 
 end
